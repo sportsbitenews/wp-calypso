sportsbitenews/wp-calypso

commit e3abadf8449eea53f22d9b1ae40422d9915618c3

Editor: Add a featured image dropzone above the post title.
This restores functionality that existed in the old editor.

The existing `MediaLibraryDropZone` is adjusted to make room for the new dropzone, because the `MediaLibraryDropZone` previously took up the entire editor pane.

The `line-height` was removed from `.editor-featured-image` because it was causing the text and icon to be squished, and didn't seem to be needed anywhere else in Calypso.

Fixes #13277

client/components/tinymce/index.jsx
@@ -522,8 +522,18 @@ module.exports = React.createClass( {
 			'is-visible': mode === 'html'
 		} );
 
+		/*
+		 * Using `classnames()` here is just a hack to avoid the linter complaining that the
+		 * container is named `tinymce-container` instead of `tinymce`. Ideally the containing
+		 * `div` and the `textarea` should be refactored so that the `div` has the `tinymce`
+		 * class, but that would interfere with higher priority fixes. This component is slated
+		 * for some refactoring in the near future, so that will be a more convenient time to
+		 * clean this up.
+		 */
+		const containerClassName = classnames( 'tinymce-container' );
+
 		return (
-			<div>
+			<div className={ containerClassName }>
 				{ 'html' === mode && config.isEnabled( 'post-editor/html-toolbar' ) &&
 					<EditorHtmlToolbar
 						content={ this.refs.text }

client/post-editor/editor-html-toolbar/index.jsx
@@ -684,6 +684,7 @@ export class EditorHtmlToolbar extends Component {
 				<MediaLibraryDropZone
 					onAddMedia={ this.onFilesDrop }
 					site={ site }
+					fullScreen={ false }
 				/>
 
 				<SimplePaymentsDialog

client/post-editor/style.scss
@@ -133,6 +133,21 @@
 	}
 }
 
+/*
+ * Constrain the `MediaLibraryDropZone` to the editor content area, instead of taking up the entire
+ * screen. This allows room for the `FeaturedImageDropZone`.
+ *
+ * It's only done while an image is actively being dragged, because it interferes with TinyMCE
+ * toolbar pinning/unpinning.
+ */
+.post-editor__content-editor .editor-featured-image.has-active-drop-zone ~ div.tinymce-container {
+	position: relative;
+}
+
+.post-editor .tinymce-container .drop-zone.is-active {
+	top: 80px; /* Move it below the TinyMCE toolbar, because it looks bad when they overlap */
+}
+
 .post-editor .tinymce {
 	min-height: 100vh;
 	resize: none;

client/components/drop-zone/style.scss
@@ -65,11 +65,13 @@
 	font-size: 30px;
 }
 
+.editor-drawer-well .drop-zone.editor-featured-image__dropzone {
+	margin: 0;
+}
+
 .drop-zone.editor-featured-image__dropzone {
 	background-color: rgba( $orange-jazzy, 0.7 );
 	border: 6px solid $orange-jazzy;
-
-	margin: 0;
 
 	&.is-dragging-over-element {
 		background-color: rgba( lighten( $orange-jazzy, 20 ), 0.8 );

client/post-editor/editor-featured-image/index.jsx
@@ -18,6 +18,8 @@ import PostActions from 'lib/posts/actions';
 import PostUtils from 'lib/posts/utils';
 import * as stats from 'lib/posts/stats';
 import EditorFeaturedImagePreviewContainer from './preview-container';
+import FeaturedImageDropZone from 'post-editor/editor-featured-image/dropzone';
+import isDropZoneVisible from 'state/selectors/is-drop-zone-visible';
 import Button from 'components/button';
 import RemoveButton from 'components/remove-button';
 import { getMediaItem } from 'state/selectors';
@@ -29,6 +31,8 @@ import { recordTracksEvent } from 'state/analytics/actions';
 class EditorFeaturedImage extends Component {
 	static propTypes = {
 		featuredImage: PropTypes.object,
+		hasDropZone: PropTypes.bool,
+		isDropZoneVisible: PropTypes.bool,
 		maxWidth: PropTypes.number,
 		site: PropTypes.object,
 		post: PropTypes.object,
@@ -39,8 +43,10 @@ class EditorFeaturedImage extends Component {
 	};
 
 	static defaultProps = {
+		hasDropZone: false,
+		isDropZoneVisible: false,
 		maxWidth: 450,
-		onImageSelected: () => {}
+		onImageSelected: () => {},
 	};
 
 	state = {
@@ -135,7 +141,8 @@ class EditorFeaturedImage extends Component {
 		const { site, post } = this.props;
 		const featuredImageId = getFeaturedImageId( post );
 		const classes = classnames( 'editor-featured-image', {
-			'is-assigned': !! PostUtils.getFeaturedImageId( this.props.post )
+			'is-assigned': PostUtils.getFeaturedImageId( this.props.post ),
+			'has-active-drop-zone': this.props.hasDropZone && this.props.isDropZoneVisible,
 		} );
 
 		return (
@@ -157,8 +164,10 @@ class EditorFeaturedImage extends Component {
 							icon="pencil"
 							className="editor-featured-image__edit-icon" />
 					</Button>
-					{ featuredImageId ? <RemoveButton onRemove={ EditorFeaturedImage.removeImage } /> : '' }
+					{ featuredImageId && <RemoveButton onRemove={ EditorFeaturedImage.removeImage } /> }
 				</div>
+
+				{ this.props.hasDropZone && <FeaturedImageDropZone /> }
 			</div>
 		);
 	}
@@ -172,6 +181,7 @@ export default connect(
 
 		return {
 			featuredImage: getMediaItem( state, siteId, featuredImageId ),
+			isDropZoneVisible: isDropZoneVisible( state, 'featuredImage' ),
 		};
 	},
 	{

client/post-editor/editor-featured-image/style.scss
@@ -2,7 +2,18 @@
 	width: 100%;
 	padding: 0 0 7px 0;
 	text-align: center;
-	line-height: 0;
+}
+
+.editor-featured-image.has-active-drop-zone {
+	display: block;
+	position: relative;
+	min-height: 115px;
+	padding: 15px 0 20px;
+}
+
+.editor-featured-image__dropzone {
+	margin-top: 0;
+	margin-bottom: 0;
 }
 
 .editor-featured-image__current-image {

client/post-editor/post-editor.jsx
@@ -387,7 +387,8 @@ export const PostEditor = React.createClass( {
 								<FeaturedImage
 									site={ site }
 									post={ this.state.post }
-									maxWidth={ 1462 } />
+									maxWidth={ 1462 }
+									hasDropZone={ true } />
 								<div className="post-editor__header">
 									<EditorTitle
 										onChange={ this.onEditorTitleChange }

commit 5fd52504bda348da1c81210e148b2dcde60467b3

Editor: Style HTML-mode dropzone to cover entire editor body
A bug was introduced in #16819 which collapsed the editor's dropzone when in HTML-editing mode. Before that PR, the dropzone was full-screen, but now it needs to be constrained to the editor body, in order to leave room for the featured-image dropzones.

Fixes #18535

client/components/tinymce/index.jsx
@@ -542,14 +542,17 @@ module.exports = React.createClass( {
 		} );
 
 		/*
-		 * Using `classnames()` here is just a hack to avoid the linter complaining that the
+		 * Using `classnames()` here is partly a hack to avoid the linter complaining that the
 		 * container is named `tinymce-container` instead of `tinymce`. Ideally the containing
 		 * `div` and the `textarea` should be refactored so that the `div` has the `tinymce`
 		 * class, but that would interfere with higher priority fixes. This component is slated
 		 * for some refactoring in the near future, so that will be a more convenient time to
 		 * clean this up.
 		 */
-		const containerClassName = classnames( 'tinymce-container' );
+		const containerClassName = classnames(
+			'tinymce-container',
+			'editor-mode-' + mode
+		);
 
 		return (
 			<div className={ containerClassName }>

client/post-editor/editor-html-toolbar/index.jsx
@@ -36,6 +36,7 @@ import AddImageDialog from './add-image-dialog';
 import AddLinkDialog from './add-link-dialog';
 import Button from 'components/button';
 import ContactFormDialog from 'components/tinymce/plugins/contact-form/dialog';
+import isDropZoneVisible from 'state/selectors/is-drop-zone-visible';
 import EditorMediaModal from 'post-editor/editor-media-modal';
 import MediaLibraryDropZone from 'my-sites/media-library/drop-zone';
 import config from 'config';
@@ -53,6 +54,7 @@ export class EditorHtmlToolbar extends Component {
 		fieldAdd: PropTypes.func,
 		fieldRemove: PropTypes.func,
 		fieldUpdate: PropTypes.func,
+		isDropZoneVisible: PropTypes.bool.isRequired,
 		moment: PropTypes.func,
 		onToolbarChangeContent: PropTypes.func,
 		settingsUpdate: PropTypes.func,
@@ -522,6 +524,7 @@ export class EditorHtmlToolbar extends Component {
 			'is-pinned': this.state.isPinned,
 			'is-scrollable': this.state.isScrollable,
 			'is-scrolled-full': this.state.isScrolledFull,
+			'has-active-drop-zone': this.props.isDropZoneVisible,
 		} );
 		const insertContentClasses = classNames( 'editor-html-toolbar__insert-content-dropdown', {
 			'is-visible': this.state.showInsertContentMenu,
@@ -693,6 +696,7 @@ export class EditorHtmlToolbar extends Component {
 
 const mapStateToProps = state => ( {
 	contactForm: get( state, 'ui.editor.contactForm', {} ),
+	isDropZoneVisible: isDropZoneVisible( state ),
 	site: getSelectedSite( state ),
 } );
 

client/post-editor/editor-html-toolbar/style.scss
@@ -85,6 +85,10 @@
 		width: 0;
 	}
 
+	&.has-active-drop-zone {
+		position: static; /* Allow the dropzone to overflow the toolbar and expand to fill the entire editor body */
+	}
+
 	.button {
 		border-right: 1px solid lighten( $gray, 30% );
 		color: darken( $gray, 20% );

client/post-editor/style.scss
@@ -144,8 +144,12 @@
 	position: relative;
 }
 
-.post-editor .tinymce-container .drop-zone.is-active {
-	top: 80px; /* Move it below the TinyMCE toolbar, because it looks bad when they overlap */
+.post-editor .tinymce-container.editor-mode-tinymce .drop-zone.is-active {
+	top: 75px; /* Move it below the TinyMCE toolbar, because it looks bad when they overlap */
+}
+
+.post-editor .tinymce-container.editor-mode-html .drop-zone.is-active {
+	top: 40px; /* Move it below the TinyMCE toolbar, because it looks bad when they overlap */
 }
 
 .post-editor .tinymce {